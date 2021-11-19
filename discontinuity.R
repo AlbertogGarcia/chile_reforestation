ff174cd455d2f9c19d4cd6671085d02fadfcafb9
Alberto Garcia <agarcia@esm.ucsb.edu>
2021-11-19 15:22:24 -0800

    adding 2021 estimates

--- discontinuity.R
+++ discontinuity.R
@@ -1,14 +1,53 @@
+
+library(reshape2)
 library(tidyverse)  # ggplot(), %>%, mutate(), and friends
 library(broom)  # Convert models to data frames
 library(rdrobust)  # For robust nonparametric regression discontinuity
-library(rddensity)  # For nonparametric regression discontinuity density tests
+library(estimatr)  # Run 2SLS models in one step with iv_robust()
 library(modelsummary)  # Create side-by-side regression tables
+library(kableExtra)  # Fancy table formatting
 library(ggplot2)
+library(Hmisc)
 library(bunchr)
 regions_200 <- c(5,6,7,8,9, 10,14)
 regions_500 <- c(1, 2, 3, 4, 15)
 regions_800 <- c(11, 12)
 NFL_df <- readRDS("C:/Users/agarcia/Dropbox/chile_collab/input_files/NFL_df.rds")
+
+
+
+
+library(readxl)
+
+# xls files downloaded from CONAF
+#projects
+proyecto_df <- read_xlsx("C:/Users/agarcia/Dropbox/chile_reforestation/external_data/concurso_conaf/program/proyecto.xlsx")
+
+#properties and coordinates
+predio_df <- read_xlsx("C:/Users/agarcia/Dropbox/chile_reforestation/external_data/concurso_conaf/program/predio.xlsx")
+#owners
+propietario_df <- read_xlsx("C:/Users/agarcia/Dropbox/chile_reforestation/external_data/concurso_conaf/program/propietario.xlsx")
+#stands
+rodal_df <- read_xlsx("C:/Users/agarcia/Dropbox/chile_reforestation/external_data/concurso_conaf/program/rodal.xlsx")
+#activities
+actividades_df <- read_xlsx("C:/Users/agarcia/Dropbox/chile_reforestation/external_data/concurso_conaf/program/actividad.xlsx")
+
+property_df <- proyecto_df %>%
+  full_join(predio_df, by = "rptpro_id") %>%
+  full_join(propietario_df, by = "rptpre_id")
+
+activity_df <- property_df %>%
+  left_join(rodal_df, by = "rptpre_id") %>%
+  left_join(actividades_df, by = "rptro_id") %>%
+  dcast(rptpro_id
+        + rptpro_ano
+        ~ rptac_tipo, 
+        fun.aggregate = function(x){as.integer(length(x) > 0)})
+
+NFL_df <- property_df %>%
+  full_join(activity_df, by = c("rptpro_id", "rptpro_ano")) 
+
+
 
 discontinuity_main <- NFL_df %>%
   rename(property_size = rptpre_superficie_predial)%>%
@@ -19,12 +58,14 @@ discontinuity_main <- NFL_df %>%
          ),
          size_centered = property_size - size_cutoff,
          below_cutoff = property_size <= size_cutoff,
-         smallholder = ifelse(rptpro_tipo_concurso == "Otros Interesados", 0, 1)
+         smallholder = ifelse(rptpro_tipo_concurso == "Otros Interesados", 0, 1),
+         price_per_hectare = rptpro_monto_total/property_size,
+         proportion_bonusarea = rptpro_superficie/property_size
   )%>%
-  group_by(rptpro_id)%>%
+  # group_by(rptpro_id)%>%
   # mutate_at(.vars = vars(anillado:zanja),
   #           .funs = list(~ max(.)))
-  ungroup()%>%
+  # ungroup()%>%
 distinct(rptpro_id, .keep_all = TRUE)
 
 #################################################################################################
@@ -41,6 +82,23 @@ discontinuity_main200 <- subset(discontinuity_main, rptpro_numero_region %in% re
 discontinuity_main800 <- subset(discontinuity_main, rptpro_numero_region %in% regions_800)
 reported_200 <- subset(discontinuity_main, between(property_size, 198, 200))%>%
   select(rptpro_id, rptpro_ano, property_size, rptpro_tipo_presenta, rptpro_tipo_concurso)
+
+#################################################################################################
+### visualize fuzzy gap
+#################################################################################################
+ggplot(discontinuity_main, aes(x = size_centered, y = proportion_bonusarea, color = rptpro_tipo_concurso)) +
+  geom_point(size = 1.5, alpha = 0.5) + 
+  # Add a line based on a linear model for the people scoring less than 70
+  geom_smooth(data = filter(discontinuity_main, size_centered <= 0), method = "lm") +
+  # Add a line based on a linear model for the people scoring 70 or more
+  geom_smooth(data = filter(discontinuity_main, size_centered > 0), method = "lm") +
+  geom_vline(xintercept = 0) +
+  labs(x = "property size (centered)", y = "proportion of property enrolled")+
+  xlim(-100, 100)+ylim(0, 1)+theme_minimal()
+
+
+
+
 #################################################################################################
 ### Density tests show that there is manipulation at the 200 hectare threshold
 #################################################################################################
@@ -83,21 +141,6 @@ ggplot(data = discontinuity_main200) +
   #scale_fill_manual(values=c("grey20", "grey60")) + 
   theme_minimal()+
   xlim(190, 210)
-
-
-
-count_with_bins <- discontinuity_main200 %>%
-  mutate(size_binned = cut(property_size, breaks = seq(0, 500, 2.5)),
-         smallholder = ifelse(rptpro_tipo_concurso == "Otros Interesados", FALSE, TRUE)) %>% 
-  # Group by each of the new bins and tutoring status
-  group_by(size_binned) %>% 
-  # Count how many people are in each test bin + used/didn't use tutoring
-  summarize(n = n())%>%
-  mutate(bin_end = as.numeric(size_binned)*2.5)%>%
-  drop_na(size_binned)%>%
-  filter(between(bin_end, 0, 75))
-
-
 
 ggplot(count_with_bins, aes(x = bin_end, y = n)) +
   geom_point(fill = "grey90", color = "blue") +
@@ -170,7 +213,7 @@ ggplot(discontinuity_with_bins2, aes(x = bin_end, y = prob_smallholder)) +
 ### characteristics jump at heaps
 #################################################################################################
 #heaps <- c(10, 20, 50, 100, 150, 250)
-heaps <- c(seq(from = 10, to = 100, by = 10), seq(from = 110, to = 250, by = 20), 300, 400, 500)
+heaps <- c(seq(from = 10, to = 50, by = 10), 70, 100, 150, 200, 300, 400, 500)
 
 # outcomes:
 # received_bonus
@@ -200,7 +243,7 @@ heaps_results <- data.frame()
 for(i in heaps){
   
 bw_side = ifelse(i < 100, 9.99, 
-                 ifelse(i > 200, 49.99, 24.99)
+                 ifelse(i >= 200, 99.99, 49.99)
                  )  
   
 df_heap <- analysis_df %>%
@@ -239,42 +282,57 @@ library(estimatr)
 ### parametric fuzzy rdd w/ received_bonus outcome
 #################################################################################################
 
-analysis_df <- discontinuity_main %>%
+analysis_df <- discontinuity_main200 %>%
   mutate(regeneration = (regeneracion ) > 0,
          planting = (`siembra-directa`
                      + plantacion + `plantacion-suplementaria`
                       + enriquecimiento
                           ) > 0,
+         reforest = (regeneration + planting) > 0,
          cutting = (`corta-liberacion` + `corta-mejoramiento` + `corta-recuperacion` +`corta-regeneracion` + `corta-selectiva` + `corta-sanitaria`) > 0,
          timber = ifelse(rptpro_objetivo_manejo == "PRODUCCION MADERERA", 1, 0),
          ecological_recovery = ifelse(rptpro_objetivo_manejo != "PRODUCCION MADERERA" & rptpro_objetivo_manejo != "PRODUCCION NO MADERERA", 1, 0),
          nontimber = ifelse(rptpro_objetivo_manejo == "PRODUCCION NO MADERERA", 1, 0)
-  )%>%
-  filter(property_size != 200 & property_size != 150 & property_size != 250 & property_size != 100)
+  )
+
+bw_list = c(120, 80, 60)
+heaps <- c(seq(from = 10, to = 50, by = 10), 70, 100, 150, 200)#, 300, 400, 500, 600, 700, 800)
 
 
-bw_list = c(75, 50, 30)
 
 rdd_results <- data.frame()
 
 for(k in bw_list){
     
-    i = "drop heap points"
+  donut = 3
+  
+    
+    donut_df <- analysis_df %>%
+      mutate(drop = ifelse(property_size %in% heaps | between(size_centered, -donut, 0), 1, 0)
+             )%>%
+      filter(drop != 1)
+      
+      
     bw = k 
+    
+    #proportion_bonusarea
+    #rptpro_monto_total
+    #timber
+    #
 
     rdd <- iv_robust(
-      received_bonus ~ size_centered + smallholder | size_centered + below_cutoff,
+      timber ~ size_centered  + smallholder | size_centered  + below_cutoff,
       fixed_effects = ~ rptpre_region , 
       #diagnostics = TRUE,
-      data = filter(analysis_df, size_centered >= -bw & size_centered <= bw & rptpro_ano < 2019)
+      data = filter(donut_df, size_centered >= -bw - donut & size_centered <= bw )
     )
     
     rdd_results <- data.frame(
       "outcome" = rdd$outcome, "coeff" = rdd$coefficients['smallholder'], "se" = rdd$std.error['smallholder'], "p.val" = rdd$p.value['smallholder'],
-      "bw" = k, "donut" = i
+      "bw" = k, "donut" = donut
     )%>%
       rbind(rdd_results)
-
+    
   }
 
 library(rio)
@@ -284,6 +342,7 @@ export(rdd_results, "rdresults_main.rds")
 ### EVI as outcome
 ###############################################################################
 library(sf)
+NFL_df <- readRDS("C:/Users/agarcia/Dropbox/chile_collab/input_files/NFL_df.rds")
 my_rol_match <- data.frame(readRDS("C:/Users/agarcia/Dropbox/chile_reforestation/data/analysis/my_rol_match.rds"))%>%
   mutate(match_type = "rol")
 my_spatial_match <- data.frame(readRDS("C:/Users/agarcia/Dropbox/chile_reforestation/data/analysis/my_spatial_match.rds"))%>%
@@ -310,7 +369,7 @@ checking_manipulation <- rol_priority %>%
   select(rptpro_tipo_concurso, rptpre_superficie_predial, area_ha, NOM_PREDIO, PROPIETARI, rptpre_nombre, rptprop_nombre, rptpro_id, evi_2007, evi_2020)
 
 bunchers_200 <- rol_priority %>%
-  filter(between(rptpre_superficie_predial, 198, 200) )%>%
+  filter(between(rptpre_superficie_predial, 190, 200) )%>%
   mutate(over = ifelse(area_ha > 200, 1, 0),
          timber = ifelse(rptpro_objetivo_manejo == "PRODUCCION MADERERA", 1, 0),
          ecological_recovery = ifelse(rptpro_objetivo_manejo == "PRODUCCION NO MADERERA", 1, 0),

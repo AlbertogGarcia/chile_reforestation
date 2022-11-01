ea784308d2a09bab87c1166d6ec064d2c07ec326
albertoggarcia <garcia.albertg@gmail.com>
2022-10-31 23:09:19 -0700

    updating draft intro

--- analysis/analysis_applications.R
+++ analysis/analysis_applications.R
@@ -3,18 +3,25 @@ library(tidyverse)
 library(reshape2)
 library(ggplot2)
 library(stringi)
+library(fixest)
+library(modelsummary)
+library(kableExtra)
+
+file_dir <- "C:/Users/garci/Dropbox/chile_reforestation/"
+results_dir <- "paper/results/"
+
 # xls files downloaded from CONAF
 #projects
-proyecto_df <- read_xlsx("C:/Users/garci/Dropbox/chile_reforestation/external_data/concurso_conaf/program/proyecto.xlsx")
+proyecto_df <- read_xlsx(paste0(file_dir, "external_data/concurso_conaf/program/proyecto.xlsx"))
 
 #properties and coordinates
-predio_df <- read_xlsx("C:/Users/garci/Dropbox/chile_reforestation/external_data/concurso_conaf/program/predio.xlsx")
+predio_df <- read_xlsx(paste0(file_dir, "external_data/concurso_conaf/program/predio.xlsx"))
 #owners
-propietario_df <- read_xlsx("C:/Users/garci/Dropbox/chile_reforestation/external_data/concurso_conaf/program/propietario.xlsx")
+propietario_df <- read_xlsx(paste0(file_dir, "external_data/concurso_conaf/program/propietario.xlsx"))
 #stands
-rodal_df <- read_xlsx("C:/Users/garci/Dropbox/chile_reforestation/external_data/concurso_conaf/program/rodal.xlsx")
+rodal_df <- read_xlsx(paste0(file_dir, "external_data/concurso_conaf/program/rodal.xlsx"))
 #activities
-actividades_df <- read_xlsx("C:/Users/garci/Dropbox/chile_reforestation/external_data/concurso_conaf/program/actividad.xlsx")
+actividades_df <- read_xlsx(paste0(file_dir, "external_data/concurso_conaf/program/actividad.xlsx"))
 
 property_df <- proyecto_df %>%
   full_join(predio_df, by = "rptpro_id") %>%
@@ -68,21 +75,6 @@ NFL_df <- property_df %>%
 pct_compliance <- mean(NFL_df$received_bonus)
 
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-##### Property size Histogram
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-ggplot(property_df, aes(x=rptpre_superficie_predial, fill = `Contest type`)) + 
-  #geom_histogram(binwidth = 5) +
-  geom_density(alpha = .5)+
-  xlab("Property size (ha)")+
-  theme_minimal()+
-  #geom_vline(xintercept = 50, linetype = "dashed")+
-  scale_x_continuous(breaks = c(0, 20, 50, 100, 200, 400, 600), limits = c(0, 700))+
-  ggtitle("Distribution of applicant property sizes")
-
-
-ggsave(width = 8, height = 5, path = "paper/figs", filename = "psize_density.png", dpi = 500)
 
 smallholder_sizes <- (property_df %>% filter(`Contest type` == "Smallholder"))$rptpre_superficie_predial
 
@@ -92,6 +84,10 @@ pct_smallholders_above_50ha <- length(smallholder_sizes[smallholder_sizes >= 50]
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ##### Recreating social and project score components
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+comunal_pov <- read.csv("C:/Users/garci/Dropbox/chile_reforestation/data/analysis_lc/comunal_poverty.csv", encoding = "Latin-1")%>%
+  mutate(comuna = tolower(accents(comuna)))%>%
+  mutate_at(vars(cpov_pct_2007), ~as.numeric(as.character(.)))
+
 
 sizecut_points <- c(0, 40, 80, 120, 200)
 bonuscut_points <- c(0, 70, 150, 300, 500)
@@ -111,7 +107,9 @@ small_df <- NFL_df %>%
          VPS = ifelse(is.na(rptprop_razon_social), 0, 100),
          adjusted_puntaje = rptpro_puntaje - .2*VI - .35*VP - 0.05*VPS,
          social_puntaje = .2*VI + .35*VP + 0.05*VPS,
-         smallholder = 1) 
+         smallholder = 1) %>%
+  mutate(comuna = tolower(accents(rptpre_comuna)))%>%
+  left_join(comunal_pov, by = "comuna")
 
 sizecut_points <- c(0, 300, 600, 2000)
 bonuscut_points <- c(0, 300, 600, 1000)
@@ -131,38 +129,148 @@ other_df <- NFL_df %>%
          adjusted_puntaje = rptpro_puntaje - .25*VI - .25*VP - 0.05*VPS,
          social_puntaje = .25*VI + .25*VP + 0.05*VPS,
          smallholder = 0
-  )
+  )%>%
+  mutate(comuna = tolower(accents(rptpre_comuna)))%>%
+  left_join(comunal_pov, by = "comuna")
+
 
 NFL_df <- rbind(other_df, small_df)
-library(rio)
-#export(NFL_df, "compliance_df.rds")
 
-compliance_mod1 <- lm(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) 
-                      + reforest  + as.factor(rptpre_region) + as.factor(rptpro_objetivo_manejo)
-                      , data = NFL_df)
-compliance_mod2 <- lm(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) + extensionista 
-                      + reforest + as.factor(rptpre_region)+ as.factor(rptpro_objetivo_manejo)
-                      , data = NFL_df)
-compliance_mod3 <- lm(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) + extensionista + smallholder + smallholder:log(social_puntaje + 1)
-                      + reforest  + as.factor(rptpre_region)+ as.factor(rptpro_objetivo_manejo)
-                      , data = NFL_df)
-compliance_mod4 <- lm(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) + extensionista + smallholder + smallholder:log(social_puntaje + 1)
-                      + reforest  + as.factor(rptpro_objetivo_manejo)
-                      , data = NFL_df)
-save(compliance_mod1, compliance_mod2, compliance_mod3, compliance_mod4, file="paper/results/compliance_models.RData")
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+### Does score predict poverty
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+score_pov <- feols(log(cpov_pct_2007 + 1) ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) 
+                    , data = NFL_df)
+summary(score_pov, vcov = ~rptpre_id)
+
+score_pov_smallholder <- feols(log(cpov_pct_2007 + 1) ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) 
+                    , data = NFL_df %>% filter(rptpro_tipo_concurso != "Otros Interesados"))
+summary(score_pov_smallholder, vcov = ~rptpre_id)
+
+score_povfe <- feols(log(cpov_pct_2007 + 1) ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) 
+                      + as.factor(rptpre_region) 
+                   #  + as.factor(rptpro_objetivo_manejo)
+                   , data = NFL_df)
+
+score_povfe_smallholder <- feols(log(cpov_pct_2007 + 1) ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) 
+                                 + as.factor(rptpre_region) 
+                               #+ as.factor(rptpro_objetivo_manejo)
+                               , data = NFL_df %>% filter(rptpro_tipo_concurso != "Otros Interesados"))
 
 
-ext_mod1 <- lm(extensionista  ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) 
-               + reforest +  as.factor(rptpre_region) + as.factor(rptpro_objetivo_manejo)
-               , data = NFL_df)
-ext_mod2 <- lm(extensionista  ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) + smallholder*log(social_puntaje + 1) 
-               + reforest +  as.factor(rptpre_region)+ as.factor(rptpro_objetivo_manejo)
-               , data = NFL_df)
-ext_mod3 <- lm(extensionista  ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1)
-               + reforest +  as.factor(rptpre_region) + as.factor(rptpro_objetivo_manejo)
-               , data = small_df)
-ext_mod4 <- lm(extensionista  ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1)
-               + reforest + as.factor(rptpro_objetivo_manejo)
-               , data = small_df)
+models = list( "(1)" = score_pov,
+               "(2)" = score_pov_smallholder,
+               "(3)" = score_povfe,
+               "(4)" = score_povfe_smallholder
+)
 
-save(ext_mod1, ext_mod2, ext_mod3, ext_mod4, file="paper/results/extension_models.RData")
+rows <- tribble(~term, ~mod1,  ~mod2, ~mod3, ~mod4,
+                'Subsample', 'Full sample', 'Smallholder', 'Full sample', 'Smallholder',
+                'Region FE', 'No', 'No', 'Yes', 'Yes'
+)%>%
+  as.data.frame()
+
+my_coef_names <- c("log(social_puntaje + 1)"= "ln(Social score)",
+                   "log(adjusted_puntaje + 1)" = "ln(Project score)"
+)
+
+options("modelsummary_format_numeric_latex" = "plain")
+
+modelsummary(models,
+             output="latex",
+             title = 'Social score is associated with relatively higher comuna level poverty',
+             fmt = 5, # 4 digits and trailing zero
+             vcov = ~rptpre_id,
+             stars = c('*' = .1, '**' = .05, '***' = .01),
+             coef_rename = my_coef_names,
+             coef_omit = 'factor|Inter',
+             gof_omit = 'DF|Deviance|Adj|Within|Pseudo|BIC|Year|FE|Std|RMSE'
+             , add_rows = rows
+             , notes = "Standard errors are clustered at the property level."
+)%>%
+  kable_styling(latex_options = c("hold_position"))%>%
+  add_header_above(c("Outcome var." = 1, "ln(ComunaPov)" =4 ) ) %>%
+  kableExtra::save_kable(paste0(results_dir, "poverty-score_assoc.tex"))
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### Determinants of compliance
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+compliance_mod1 <- feols(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) +
+                           smallholder +
+                       + as.factor(rptpre_region) 
+                     #  + as.factor(rptpro_objetivo_manejo)
+                      , data = NFL_df)
+summary(compliance_mod1)
+
+compliance_mod2 <- feols(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) +
+                               log(cpov_pct_2007 + 1)
+                         +smallholder
+                         + as.factor(rptpre_region) 
+                     #    + as.factor(rptpro_objetivo_manejo)
+                         , data = NFL_df)
+
+compliance_mod3 <- feols(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) +
+                           log(cpov_pct_2007 + 1)
+                         + log(social_puntaje + 1)*smallholder
+                         + as.factor(rptpre_region) 
+                     #    + as.factor(rptpro_objetivo_manejo)
+                         , data = NFL_df)
+
+compliance_mod4 <- feols(received_bonus ~ log(social_puntaje + 1) + log(adjusted_puntaje + 1) +
+                           log(cpov_pct_2007 + 1)
+                         + as.factor(rptpre_region) 
+                     #    + as.factor(rptpro_objetivo_manejo)
+                         , data = small_df)
+
+compliance_mod5 <- feols(received_bonus ~ 
+                           log(cpov_pct_2007 + 1)
+                         + log(cpov_pct_2007 + 1)*smallholder
+                          + log(rptpro_monto_total)
+                         + extensionista
+                         + as.factor(rptpre_region) 
+                     #    + as.factor(rptpro_objetivo_manejo)
+                         , data = NFL_df)
+
+models = list( "(1)" = compliance_mod1,
+               "(2)" = compliance_mod2,
+               "(3)" = compliance_mod3,
+               "(4)" = compliance_mod4,
+               "(5)" = compliance_mod5
+)
+
+rows <- tribble(~term, ~mod1,  ~mod2, ~mod3, ~mod1_log, ~mod2_log, 
+                'Subsample', 'Full sample', 'Full sample', 'Full sample', 'Smallholder','Full sample',
+                'Region FE' , 'Yes', 'Yes', 'Yes', 'Yes', 'Yes'
+)%>%
+  as.data.frame()
+
+my_coef_names <- c("log(social_puntaje + 1)"= "ln(Social score)",
+                   "log(cpov_pct_2007 + 1)" = "ln(ComunaPov)",
+                   "log(adjusted_puntaje + 1)" = "ln(Project score)",
+                   "smallholder" = "Smallholder",
+                   "extensionista" = "Extensionist",
+                   "log(social_puntaje + 1):smallholder" = "ln(Social score) x Smallholder",
+                   "log(rptpro_monto_total)" = "ln(Subsidy amount)",
+                   "log(cpov_pct_2007 + 1):smallholder" = "ln(ComunaPov) x Smallholder"
+)
+
+
+modelsummary(models,
+             output="latex",
+             title = 'Social score is negatively associated with compliance',
+             fmt = 5, # 4 digits and trailing zero
+             vcov = ~rptpre_id,
+             stars = c('*' = .1, '**' = .05, '***' = .01),
+             coef_rename = my_coef_names,
+           coef_omit = 'factor|Inter',
+             gof_omit = 'DF|Deviance|Adj|Within|Pseudo|BIC|Year|FE|Std|RMSE'
+             , add_rows = rows
+             , notes = "Standard errors are clustered at the property level."
+)%>%
+  kable_styling(latex_options = c("hold_position"))%>%
+  add_header_above(c("Outcome var." = 1, "Complied" = 5) ) %>%
+  kableExtra::save_kable(paste0(results_dir, "compliance_targeting.tex"))

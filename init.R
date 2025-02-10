efd8908ce1c1ddb891505c6b6825e4ecfc61e5cb
rheilmayr <rheilmayr@ucsb.edu>
2025-02-10 09:06:04 -0800

    adding my directories

--- init.R
+++ init.R
@@ -9,9 +9,12 @@
 
 # Define the path to your local code directory
 code_dir <- 'C:\\Users\\garci\\Documents\\chile_reforestation\\'
+# code_dir <- 'D:\\dev\\chile\\chile_reforestation\\'
+
 
 # Define the path to the local path of your dropbox folder 
 data_dir <- 'C:\\Users\\garci\\Dropbox\\chile_reforestation\\'
+# data_dir <- 'D:\\cloud\\Dropbox\\collaborations\\chile_reforestation\\'
 
 library(R.utils)
 createLink(paste0(code_dir, 'remote\\'), data_dir, overwrite = FALSE)
